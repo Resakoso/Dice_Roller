867fce8ff36624bbc9b5adec0f5da3eccff97f67
DAM2XX <dam07.2021@gmail.com>
2022-10-10 10:09:57 +0200

    initial commit

---
+++ app/src/main/java/dam/jesuitas/diceroller/MainActivity.kt
@@ -0,0 +1,38 @@
+package dam.jesuitas.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    class Dice(val numSides: Int) {
+
+        fun roll(): Int {
+            return (1..numSides).random()
+        }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+
+        val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+        toast.show()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {rollDice()}
+
+    }
+
+}
