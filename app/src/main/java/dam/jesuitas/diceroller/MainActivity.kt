ec088e3d6b2e17311bdc459e9ef3ea572d6e3be3
DAM2XX <dam07.2021@gmail.com>
2022-10-10 10:41:26 +0200

    imagenes

--- app/src/main/java/dam/jesuitas/diceroller/MainActivity.kt
+++ app/src/main/java/dam/jesuitas/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package dam.jesuitas.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -22,9 +23,25 @@ class MainActivity : AppCompatActivity() {
         val resultTextView: TextView = findViewById(R.id.textView)
         resultTextView.text = diceRoll.toString()
 
+        //Hay que referenciar la variable con el elemento del xml sobre el que va a trabajar.
+        val diceImage : ImageView = findViewById(R.id.imageView)
+
+
+        when(diceRoll){
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        }
+
         val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
         toast.show()
     }
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
